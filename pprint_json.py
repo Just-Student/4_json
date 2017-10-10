db856de44b12dc394ebd2fb6cea31070fdeda962
Just-Student <rei.kasano@gmail.com>
2017-10-11 00:43:45 +0300

    add functionality

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,16 @@ import json
 
 
 def load_data(filepath):
-    pass
+    f = open(filepath, "r", encoding="utf8")
+    pretty_print_json(json.loads(f.read()))
+    f.close()
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    json_filepath = input('Input a path to json-file:')
+    load_data(json_filepath)
+
