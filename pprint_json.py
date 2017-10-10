7b6a342b33577d89d3f0330323db1022c3d43572
Just-Student <rei.kasano@gmail.com>
2017-10-11 01:44:05 +0300

    rename variables

--- pprint_json.py
+++ pprint_json.py
@@ -2,13 +2,13 @@ import json
 
 
 def load_data(filepath):
-    f = open(filepath, "r", encoding="utf8")
-    pretty_print_json(json.loads(f.read()))
-    f.close()
+    file = open(filepath, "r", encoding="utf8")
+    pretty_print_json(json.loads(file.read()))
+    file.close()
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4))
+def pretty_print_json(json_data):
+    print(json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
